bfaedcedb578eff7ea1072616c470eb7d724e014
thekikao <me@thekikao.com>
2017-09-11 23:35:05 +0200

    . extend testing 	-> add GET /todos test 	-> refactor POST /todo tests

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,23 +1,31 @@
 const expect = require('expect');
-const request = require('supertest');
+const supertest = require('supertest');
 
 const {app} = require('./../server.js');
 const {Todo} = require('./../models/todo.js');
+
+const dummyTodos = [{
+    text: 'first test todo'
+}, {
+    text: 'second test todo'
+}];
 
 
 // run before every test case
 beforeEach((done) => {
     // wipe all todos
-    Todo.remove({}).then(() => done());
+    Todo.remove({}).then(() => {
+        return Todo.insertMany(dummyTodos);
+    }).then(() => done());
 });
 
 // define tests
 describe('POST /todos', () => {
-    it('should create a new todo', (done) => {
+    it('create a new todo', (done) => {
         var testText = 'mysterions testText';
 
         // make request with supertestText
-        request(app)
+        supertest(app)
             .post('/todos')
             .send({
                 text: testText
@@ -32,7 +40,7 @@ describe('POST /todos', () => {
                 }
 
                 // check if record is saved in database
-                Todo.find().then((todos) => {
+                Todo.find({text: testText}).then((todos) => {
                     expect(todos.length).toBe(1);
                     expect(todos[0].text).toBe(testText);
                     done();
@@ -40,8 +48,8 @@ describe('POST /todos', () => {
             });
     });
 
-    it('should not create todo with invalid data', (done) => {
-        request(app)
+    it('don\'t create a todo with invalid data', (done) => {
+        supertest(app)
             .post('/todos')
             .send({})
             .expect(400)
@@ -52,7 +60,37 @@ describe('POST /todos', () => {
 
                 // check if record is saved in database
                 Todo.find().then((todos) => {
-                    expect(todos.length).toBe(0);
+                    expect(todos.length).toBe(2);
+                    expect(todos[0].text).toBe(dummyTodos[0].text);
+                    expect(todos[1].text).toBe(dummyTodos[1].text);
+                    done();
+                }).catch((err) => done(err));
+            });
+    });
+});
+
+describe('GET /todos', () => {
+    it('get all todos', (done) => {
+        supertest(app)
+            .get('/todos')
+            .expect(200)
+            .expect((res) => {
+                // console.log('## res', res);
+                // console.log('## res.body', require('util').inspect(res.body.todos, true, 5, true));
+                expect(res.body.todos).toBe(2);
+                expect(res.body.todos[0].text).toBe(dummyTodos[0].text);
+                expect(res.body.todos[1].text).toBe(dummyTodos[1].text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                // check if record is saved in database
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(2);
+                    expect(todos[0].text).toBe(dummyTodos[0].text);
+                    expect(todos[1].text).toBe(dummyTodos[1].text);
                     done();
                 }).catch((err) => done(err));
             });
